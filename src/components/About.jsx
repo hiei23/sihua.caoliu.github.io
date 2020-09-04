ddcb49cef0339fbd36daefc2611c0878d1afaeca
Si Hua <yuikazuga_25@hotmail.co.uk>
2020-09-04 17:05:21 -0400

    updated about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 import { withStyles } from '@material-ui/core/styles';
 import { ABOUT } from './Content'
+import READING_GUY from '../images/readingGuy.png'
 
 const styles = (theme) => (
   {
     container: {
       backgroundColor: `${theme.palette.white}`,
       textAlign: 'center',
-      height: 'auto'
-    }
+      height: '1%'
+    },
+    card:{
+      marginTop: 25,
+      backgroundColor: 'transparent',
+      paddingTop: 25,
+      paddingLeft: 25,
+      paddingRight: 25,
+    },
+    cardContent:{
+      float: 'left',
+    },
+    media: {
+      height: 200,
+      width: 300,
+      float: 'right'
+    },
   }
 )
 
@@ -32,8 +51,17 @@ const About = ({classes}) => {
 
   return(
     <section className={classes.container}>
-      <Description paragraphs={paragraphs} />
-      <Quote text={quote.text} author={quote.author} />
+      <Card className={classes.card}> 
+        <CardContent className={classes.cardContent}>
+          <Description paragraphs={paragraphs} />
+          <Quote text={quote.text} author={quote.author} />
+        </CardContent>
+        <CardMedia
+          className={classes.media}
+          image={READING_GUY}
+          title="avatar"
+        />
+      </Card>
     </section>
   )
 }
